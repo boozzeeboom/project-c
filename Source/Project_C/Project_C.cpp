41916fdafb61704eca5421b0e7a677cb6615828a
Indeed <leon74rus@mail.ru>
2025-04-13 23:45:45 +0500

    пересбор

---
+++ Source/Project_C/Project_C.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_C.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_C, "Project_C" );

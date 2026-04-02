f33f8e892de203328a1127f62b88eb0664223248
Indeed <leon74rus@mail.ru>
2026-04-02 18:10:09 +0500

    Добавлен Unity Netcode for GameObjects и базовая сетевая инфраструктура

---
+++ Assets/_Project/Scripts/Player/NetworkPlayer.cs
@@ -0,0 +1,53 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ProjectC.Player
+{
+    /// <summary>
+    /// Сетевой компонент игрока
+    /// Синхронизирует позицию и состояние игрока между клиентами
+    /// </summary>
+    public class NetworkPlayer : NetworkBehaviour
+    {
+        [Header("Настройки синхронизации")]
+        [SerializeField] private float positionSyncThreshold = 0.01f;
+        
+        // NetworkObject для этого игрока
+        private NetworkObject networkObject;
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            
+            networkObject = GetComponent<NetworkObject>();
+            
+            if (IsOwner)
+            {
+                Debug.Log($"[Player] Локальный игрок spawned. OwnerClientId: {OwnerClientId}");
+            }
+            else
+            {
+                Debug.Log($"[Player] Удалённый игрок spawned. OwnerClientId: {OwnerClientId}");
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            base.OnNetworkDespawn();
+            Debug.Log($"[Player] Игрок despawned. OwnerClientId: {OwnerClientId}");
+        }
+
+        /// <summary>
+        /// Проверка: это локальный игрок?
+        /// </summary>
+        public bool IsLocalPlayer => IsOwner;
+
+        /// <summary>
+        /// Получить ClientId владельца
+        /// </summary>
+        public ulong GetOwnerId()
+        {
+            return OwnerClientId;
+        }
+    }
+}

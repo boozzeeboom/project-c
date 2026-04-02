17c281131ee952108350d281a6b4dd6e0efafc9d
Indeed <leon74rus@mail.ru>
2026-04-02 18:19:08 +0500

    Заменён FindFirstObjectByType на FindAnyObjectByType

--- Assets/_Project/Scripts/UI/NetworkUI.cs
+++ Assets/_Project/Scripts/UI/NetworkUI.cs
@@ -22,7 +22,7 @@ namespace ProjectC.UI
         private void Awake()
         {
             // Находим Network Manager
-            networkManagerController = FindFirstObjectByType<NetworkManagerController>();
+            networkManagerController = FindAnyObjectByType<NetworkManagerController>();
 
             if (networkManagerController == null)
             {

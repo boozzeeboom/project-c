07b40537165c8ad14611834021d7965b00fe8690
Indeed <leon74rus@mail.ru>
2026-04-03 18:23:16 +0500

    Шаг 2: TestPlatformCreator + фикс Shift в WorldCamera

---
+++ Assets/_Project/Scripts/Core/TestPlatformCreator.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace ProjectC.Core
+{
+    /// <summary>
+    /// Создаёт тестовую платформу для проверки персонажа
+    /// Удаляй этот скрипт когда будет готов мир
+    /// </summary>
+    public class TestPlatformCreator : MonoBehaviour
+    {
+        [Header("Настройки платформы")]
+        [Tooltip("Размер платформы")]
+        [SerializeField] private Vector3 platformSize = new Vector3(50, 1, 50);
+
+        [Tooltip("Позиция платформы")]
+        [SerializeField] private Vector3 platformPosition = new Vector3(0, 0, 0);
+
+        [Tooltip("Цвет платформы")]
+        [SerializeField] private Color platformColor = Color.gray;
+
+        [Header("Стена-ориентир")]
+        [Tooltip("Создать стену для ориентации")]
+        [SerializeField] private bool createWall = true;
+
+        private void Start()
+        {
+            CreatePlatform();
+            if (createWall) CreateWall();
+        }
+
+        private void CreatePlatform()
+        {
+            GameObject platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            platform.name = "TestPlatform";
+            platform.transform.position = platformPosition;
+            platform.transform.localScale = platformSize;
+
+            // Материал
+            Material mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+            mat.color = platformColor;
+            platform.GetComponent<Renderer>().material = mat;
+
+            Debug.Log($"[TestPlatform] Создана платформа: {platformSize}");
+        }
+
+        private void CreateWall()
+        {
+            GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            wall.name = "TestWall";
+            wall.transform.position = platformPosition + new Vector3(0, 3, -platformSize.z / 2 - 1);
+            wall.transform.localScale = new Vector3(platformSize.x, 6, 1);
+
+            Material mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+            mat.color = Color.blue;
+            wall.GetComponent<Renderer>().material = mat;
+
+            Debug.Log("[TestPlatform] Создана стена-ориентир");
+        }
+    }
+}
